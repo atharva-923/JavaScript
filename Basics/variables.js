be35328571df418da4e0a75f6f85aaa0798acf9e
atharva-923 <atharvaap9@gmail.com>
2026-08-08 14:26:19 +0530

    update

--- Basics/variables.js
+++ Basics/variables.js
@@ -3,11 +3,11 @@ let accountEmail = `atharva@gmail.com`
 var accountPassword = `12345`// prefer not to use this variable due to issue in the block and fucntion scope
 accountCity = `Virar`
 
-// accountId = 567 <--- const
+//accountId = 567 <--- const value assigned variable cannot be changed.
 accountEmail = `athaa@gmail.com`
 accountPassword = `6789`
 accountCity = `Mumbai`
 
-// console.log(accountId);
+console.log(accountId);
 
 console.table([accountId,accountEmail,accountPassword,accountCity])
