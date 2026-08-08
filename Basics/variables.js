9e6878a9fedf6b63e0daf88b2444d9ff73b5bebc
atharva-923 <atharvaap9@gmail.com>
2026-08-08 14:17:51 +0530

    update

--- Basics/variables.js
+++ Basics/variables.js
@@ -1,4 +1,4 @@
-const accountId = 1234
+const accountId = 1234 // we use const because we don't want to change the value of this variable 
 let accountEmail = `atharva@gmail.com`
 var accountPassword = `12345`// prefer not to use this variable due to issue in the block and fucntion scope
 accountCity = `Virar`
