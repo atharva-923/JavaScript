0a5d350c1b6808424288480696972ae5b8ee432e
atharva-923 <atharvaap9@gmail.com>
2026-08-12 21:24:48 +0530

    string

---
+++ Basics/string.js
@@ -0,0 +1,11 @@
+const name = "Atharva";
+
+const age = 19 ;
+
+// console.log("My name is " + name + " and I am " + age + " years old."); old method
+
+console.log(`My name is ${name} and I am ${age} years old.`); // new method
+
+const name1 = new String('Atharva');
+
+console.log(name1);
