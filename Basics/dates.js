70e19a93fbb10f54d7e1bef431936d3a7863c7d3
atharva-923 <atharvaap9@gmail.com>
2026-08-21 19:41:13 +0530

    dates

---
+++ Basics/dates.js
@@ -0,0 +1,13 @@
+let date = new Date()
+
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+
+let CreatedDate = new Date(2006 , 9 , 9)
+
+console.log(CreatedDate.toDateString());
