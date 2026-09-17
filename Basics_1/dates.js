bc7f9d01ee8e8b6e696972729bec5813706cdec4
atharva-923 <atharvaap9@gmail.com>
2026-09-17 13:03:59 +0530

    Uncomment console.log statements for the current date and its formats

--- Basics_1/dates.js
+++ Basics_1/dates.js
@@ -1,12 +1,12 @@
 let date = new Date()
 
-// console.log(date);
-// console.log(date.toString());
-// console.log(date.toDateString());
-// console.log(date.toISOString());
-// console.log(date.toJSON());
-// console.log(date.toLocaleDateString());
-// console.log(date.toLocaleString());
+console.log(date);
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toJSON());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString());
 
 let CreatedDate = new Date(2006 , 9 , 9)
 
