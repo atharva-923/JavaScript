3b95c5ed87ccba28f46fd8a26402f5097431b4a8
atharva-923 <atharvaap9@gmail.com>
2026-09-24 20:23:24 +0530

    objects

---
+++ Basics_2/objects.js
@@ -0,0 +1,12 @@
+Object.create // constructor method
+
+const person = {
+  name: 'Atharva',
+  age: 19,
+  location: 'Virar',
+  email:"atharvaap9@gmail.com",
+}
+
+console.log(person.email) // accessing the email property of the person object;
+
+console.log(person['email']) //We use '' to access the property of the object.
